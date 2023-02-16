be24fba3e24b6f843870723763ebe2c2021006a3
jcuribev <jcamilouribev@gmail.com>
2023-02-15 21:49:12 -0500

    Added env variables, function name changed, better error handling

--- Indexer/Indexer.go
+++ Indexer/Indexer.go
@@ -2,6 +2,7 @@ package Indexer
 
 import (
 	"Indexer/file"
+	"fmt"
 	"net/http"
 	"os"
 )
@@ -9,31 +10,42 @@ import (
 const bulkAddress = "http://localhost:4080/api/_bulkv2"
 const directory = "./IndexEmails/"
 
-func IndexEmails() {
+func IndexEmailsToDatabase() error {
 
-	filesInfo := file.ReadFilesFromDirectory(directory)
+	filesInfo, err := file.ReadFilesFromDirectory(directory)
+
+	if err != nil {
+		return err
+	}
 
 	for i := range filesInfo {
 
 		file, err := os.Open(directory + filesInfo[i].Name())
 		if err != nil {
-			println(err)
+			println("couldn't open file:" + file.Name())
+			fmt.Printf("err: %v\n", err)
+			continue
 		}
 		defer file.Close()
 
 		request, err := http.NewRequest("POST", bulkAddress, file)
 
 		if err != nil {
-			println(err)
+			return err
 		}
 
-		request.SetBasicAuth("admin", "Complexpass#123")
+		request.SetBasicAuth(os.Getenv("ZINC_FIRST_ADMIN_USER"), os.Getenv("ZINC_FIRST_ADMIN_PASSWORD"))
 
 		response, err := http.DefaultClient.Do(request)
 
 		if err != nil {
-			println(err)
+			println("couldn't POST file:" + file.Name())
+			fmt.Printf("err: %v\n", err)
 		}
 		defer response.Body.Close()
+
+		fmt.Printf(file.Name())
 	}
+
+	return nil
 }
