02a30697e5e224b440b45fc63592690bb5c4a38b
jcuribev <jcamilouribev@gmail.com>
2023-02-15 21:45:52 -0500

    Created method to handle program initialization

--- main.go
+++ main.go
@@ -3,54 +3,89 @@ package main
 import (
 	"Indexer/Indexer"
 	"Indexer/file"
+	"errors"
 	"flag"
-	"fmt"
-	"log"
 	"os"
 	"runtime"
 	"runtime/pprof"
 )
 
-var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
-var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+var cpuProfile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memProfile = flag.String("memprofile", "", "write memory profile to `file`")
 
 func main() {
 
 	flag.Parse()
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
-		}
-		defer f.Close() // error handling omitted for example
-		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
-		}
-		defer pprof.StopCPUProfile()
+	if err := initializeProgram(); err != nil {
+		os.Exit(1)
 	}
+}
+
+func initializeProgram() error {
+
+	cpuFile, err := handleProfileFlag(*cpuProfile)
+	if err != nil {
+		return err
+	}
+	cpuFile.Close()
+
+	if err := pprof.StartCPUProfile(cpuFile); err != nil {
+		return errors.New("could not start CPU profile: " + err.Error())
+	}
+	defer pprof.StopCPUProfile()
 
 	sourceFile := flag.Arg(0)
 
 	if sourceFile == "" {
-		fmt.Println("Missing file argument.")
-		os.Exit(1)
+		return errors.New("Missing file argument.")
 	}
 
-	fmt.Println("Selected tgz:", flag.Arg(0))
-
-	file.OpenTgzFile(sourceFile)
-
-	Indexer.IndexEmails()
-
-	if *memprofile != "" {
-		f, err := os.Create(*memprofile)
-		if err != nil {
-			log.Fatal("could not create memory profile: ", err)
-		}
-		defer f.Close() // error handling omitted for example
-		runtime.GC()    // get up-to-date statistics
-		if err := pprof.WriteHeapProfile(f); err != nil {
-			log.Fatal("could not write memory profile: ", err)
-		}
+	if err := runProgram(sourceFile); err != nil {
+		return err
 	}
+
+	memoryFile, err := handleProfileFlag(*memProfile)
+	if err != nil {
+		return err
+	}
+
+	defer memoryFile.Close()
+
+	runtime.GC()
+	if err := pprof.WriteHeapProfile(memoryFile); err != nil {
+		return errors.New("could not write memory profile: " + err.Error())
+	}
+
+	return nil
+}
+
+func runProgram(sourceFile string) error {
+
+	tarReader, err := file.GetTgzReader(sourceFile)
+	if err != nil {
+		return err
+	}
+
+	if err := Indexer.IterateTarReader(tarReader); err != nil {
+		return err
+	}
+
+	if err := Indexer.IndexEmailsToDatabase(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func handleProfileFlag(fileName string) (*os.File, error) {
+	if fileName == "" {
+		return nil, nil
+	}
+
+	f, err := file.CreateProfileFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	return f, nil
 }
