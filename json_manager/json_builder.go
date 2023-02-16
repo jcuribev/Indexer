77ed90ba417c2bcf37efaa1775dd3224a00008de
jcuribev <jcamilouribev@gmail.com>
2023-02-15 21:49:39 -0500

    better error handling

--- json_manager/json_builder.go
+++ json_manager/json_builder.go
@@ -3,24 +3,33 @@ package json_manager
 import (
 	"Indexer/email"
 	"encoding/json"
-	"fmt"
 	"os"
 )
 
-func InitFile(jsonFile *os.File) {
-	jsonFile.WriteString(`{ "index" : "emails", "records": [` + "\n")
+func InitFile(jsonFile *os.File) error {
+	_, err := jsonFile.WriteString(`{ "index" : "emails", "records": [` + "\n")
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func FinishFile(jsonFile *os.File) {
-	jsonFile.WriteString(`]}`)
+func FinishFile(jsonFile *os.File) error {
+	if _, err := jsonFile.WriteString(`]}`); err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func EmailToJson(email email.Email) []byte {
+func EmailToJson(email email.Email) ([]byte, error) {
 	jsondata, err := json.Marshal(email)
 
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 
-	return jsondata
+	return jsondata, err
 }
